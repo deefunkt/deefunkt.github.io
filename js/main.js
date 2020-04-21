2f23f9bcae160f8a2acfce187f1e8ed5ad5fdd68
deefunkt <deefunktify@gmail.com>
2020-04-21 23:46:58 +1000

    Restructuring, shifted skillcard generation to clientside, reducing the static code size to manage by several hundred lines.

--- js/main.js
+++ js/main.js
@@ -1,193 +1,137 @@
 
-            var color_array = [];
-            color_array.push(getComputedStyle(document.documentElement).getPropertyValue('--card-color4'));
-            // color_array.push(getComputedStyle(document.documentElement).getPropertyValue('--card-color2'));
-            // color_array.push(getComputedStyle(document.documentElement).getPropertyValue('--background-navbar-color'));
-            color_array.push(getComputedStyle(document.documentElement).getPropertyValue('--card-color4'));
-            var num_cards = $('.grid-item-card').length;
-            AOS.init();
-            // init Isotope
-            var $skillgrid = $('#facts .grid');
-            var $maingrid = $('.layout-grid');
-            var previousSkill = '';
+var color_array = [];
+color_array.push(getComputedStyle(document.documentElement).getPropertyValue('--card-color4'));
+// color_array.push(getComputedStyle(document.documentElement).getPropertyValue('--card-color2'));
+// color_array.push(getComputedStyle(document.documentElement).getPropertyValue('--background-navbar-color'));
+color_array.push(getComputedStyle(document.documentElement).getPropertyValue('--card-color4'));
+var num_cards = $('.grid-item-card').length;
+AOS.init();
+// init Isotope
+var $skillgrid = $('#facts .grid');
+var $maingrid = $('.layout-grid');
+var previousSkill = '';
 
-            // randomizeGridCardColors();
-
-
-            
-            function randomizeGridCardColors() {
-                var num_color;
-                for (var i = 0; i < num_cards; i++){
-                    if (i%2 == 0){
-                        num_color
-                    }
-                    num_color = Math.floor(Math.random() * color_array.length);
-                    $('.grid-item-card')[i].style.backgroundColor = color_array[i%2];
-                }
-            }
-            // assign item filter to isotope grid
-            $skillgrid.isotope({
-                itemSelector: "#facts .grid-item", 
-                layoutMode: "fitRows" ,
-                masonry: {
-                    // use element for option
-                    columnWidth: '.skill-grid-sizer'
-                }
-            });
-
-            $maingrid.isotope({
-                // set itemSelector so .grid-sizer is not used in layout
-                itemSelector: '.layout-grid-item',
-                percentPosition: true,
-                masonry: {
-                    // use element for option
-                    columnWidth: '.layout-grid-sizer'
-                }
-            });
-            // filter items on button click
-            $('#facts .filters').on( 'click', '.type', function() {
-                var filterValue = $(this).attr('data-filter');
-                $('#facts .grid').isotope({ filter: filterValue });
-                $maingrid.isotope('layout');
-                AOS.refresh();
-                // AOS.init();
-            });
-             // change is-checked class on buttons
-            $('#facts .filters').each( function( i, typeGroup ) {
-                var $typeGroup = $( typeGroup );
-                $typeGroup.on( 'click', '.type', function() {
-                    $typeGroup.find('.active').removeClass('active');
-                    $( this ).addClass('active');
-                });
-            });
-            
-            // expand skillgrid items when clicked
-            $skillgrid.on( 'click', '.grid-item-card', function() {
-
-                var itemContent = this;
-                setItemContentPixelSize( itemContent );
-
-                var itemElem = itemContent.parentNode;
-                if (previousSkill == ''){
-                    //either the first, or its been clicked before
-                    previousSkill = itemElem;
-                } else if(previousSkill == itemElem){
-                    previousSkill = '';
-                } else{
-                    $(previousSkill).toggleClass('is-expanded');
-                    previousSkill = itemElem;
-                }
-                $( itemElem ).toggleClass('is-expanded');
-
-                
-
-                // force redraw
-                var redraw = itemContent.offsetWidth;
-                // renable default transition
-                itemContent.style[ transitionProp ] = '';
-
-                addTransitionListener( itemContent );
-                setItemContentTransitionSize( itemContent, itemElem );
-
-                $skillgrid.isotope('layout');
-                $maingrid.isotope('layout');
-            });
-
-            var docElemStyle = document.documentElement.style;
-            var transitionProp = typeof docElemStyle.transition == 'string' ? 'transition' : 'WebkitTransition';
-            var transitionEndEvent = {
-                WebkitTransition: 'webkitTransitionEnd',
-                transition: 'transitionend'
-            }[ transitionProp ];
-
-            function setItemContentPixelSize( itemContent ) {
-                var previousContentSize = getSize( itemContent );
-                // disable transition
-                itemContent.style[ transitionProp ] = 'none';
-                // set current size in pixels
-                itemContent.style.width = previousContentSize.width + 'px';
-                itemContent.style.height = previousContentSize.height + 'px';
-            }
-
-            function addTransitionListener( itemContent ) {
-                if ( !transitionProp ) {
-                    return;
-                }
-                // reset 100%/100% sizing after transition end
-                var onTransitionEnd = function() {
-                    itemContent.style.width = '';
-                    itemContent.style.height = '';
-                    itemContent.removeEventListener( transitionEndEvent, onTransitionEnd );
-                };
-                itemContent.addEventListener( transitionEndEvent, onTransitionEnd );
-            }
-
-            function setItemContentTransitionSize( itemContent, itemElem ) {
-                // set new size
-                var size = getSize( itemElem );
-                itemContent.style.width = size.width + 'px';
-                itemContent.style.height = size.height + 'px';
-            }
+// randomizeGridCardColors();
 
 
-            // Smooth scroll for the navigation menu and links with .scrollto classes
-            $(document).on('click', '.navbar-nav a, .scrollto', function(e) {
-                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                e.preventDefault();
-                var target = $(this.hash);
-                if (target.length) {
 
-                    var scrollto = target.offset().top;
+function randomizeGridCardColors() {
+    var num_color;
+    for (var i = 0; i < num_cards; i++){
+        if (i%2 == 0){
+            num_color
+        }
+        num_color = Math.floor(Math.random() * color_array.length);
+        $('.grid-item-card')[i].style.backgroundColor = color_array[i%2];
+    }
+}
 
-                    $('html, body').animate({
-                    scrollTop: scrollto
-                    }, 1500, 'easeInOutExpo');
+// Generate skillcards goes here
+generateSkillCards();
+function initializeIsotopes(){
+    
+}
+// assign item filter to isotope grid
+$skillgrid.isotope({
+    itemSelector: "#facts .grid-item", 
+    layoutMode: "fitRows" ,
+    masonry: {
+        // use element for option
+        columnWidth: '.skill-grid-sizer'
+    }
+});
 
-                    if ($(this).parents('.navbar-nav, .mobile-nav').length) {
-                    $('.navbar-nav .active, .mobile-nav .active').removeClass('active');
-                    $(this).closest('li').addClass('active');
-                    }
+$maingrid.isotope({
+    // set itemSelector so .grid-sizer is not used in layout
+    itemSelector: '.layout-grid-item',
+    percentPosition: true,
+    masonry: {
+        // use element for option
+        columnWidth: '.layout-grid-sizer'
+    }
+});
+// filter items on button click
+$('#facts .filters').on( 'click', '.type', function() {
+    var filterValue = $(this).attr('data-filter');
+    $('#facts .grid').isotope({ filter: filterValue });
+    $maingrid.isotope('layout');
+    AOS.refresh();
+    // AOS.init();
+});
+// change is-checked class on buttons
+$('#facts .filters').each( function( i, typeGroup ) {
+    var $typeGroup = $( typeGroup );
+    $typeGroup.on( 'click', '.type', function() {
+        $typeGroup.find('.active').removeClass('active');
+        $( this ).addClass('active');
+    });
+});
 
-                    if ($('body').hasClass('mobile-nav-active')) {
-                    $('body').removeClass('mobile-nav-active');
-                    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-                    }
-                    return false;
-                }
-                }
-            });
+// expand skillgrid items when clicked
+$skillgrid.on( 'click', '.grid-item-card', function() {
 
-            $(document).on('click', '.mobile-nav-toggle', function(e) {
-                $('body').toggleClass('mobile-nav-active');
-                $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-            });
+    var itemContent = this;
+    setItemContentPixelSize( itemContent );
 
-            $(document).click(function(e) {
-                var container = $(".mobile-nav-toggle");
-                if (!container.is(e.target) && container.has(e.target).length === 0) {
-                if ($('body').hasClass('mobile-nav-active')) {
-                    $('body').removeClass('mobile-nav-active');
-                    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-                }
-                }
-            });
+    var itemElem = itemContent.parentNode;
+    insertSkillDescription(itemElem);
 
-            // Navigation active state on scroll
-            var nav_sections = $('section');
-            var main_nav = $('.nav-item, #mobile-nav');
+    if (previousSkill == ''){
+        //either the first, or its been clicked before
+        previousSkill = itemElem;
+    } else if(previousSkill == itemElem){
+        previousSkill = '';
+    } else{
+        $(previousSkill).toggleClass('is-expanded');
+        previousSkill = itemElem;
+    }
+    $( itemElem ).toggleClass('is-expanded');
 
-            $(window).on('scroll', function() {
-                var cur_pos = $(this).scrollTop() + 10;
+    // force redraw
+    var redraw = itemContent.offsetWidth;
+    // renable default transition
+    itemContent.style[ transitionProp ] = '';
 
-                nav_sections.each(function() {
-                var top = $(this).offset().top,
-                    bottom = top + $(this).outerHeight();
+    addTransitionListener( itemContent );
+    setItemContentTransitionSize( itemContent, itemElem );
 
-                if (cur_pos >= top && cur_pos <= bottom) {
-                    if (cur_pos <= bottom) {
-                    main_nav.find('li').removeClass('active');
-                    }
-                    main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-                }
-                });
-            });
+    $skillgrid.isotope('layout');
+    $maingrid.isotope('layout');
+});
+
+var docElemStyle = document.documentElement.style;
+var transitionProp = typeof docElemStyle.transition == 'string' ? 'transition' : 'WebkitTransition';
+var transitionEndEvent = {
+    WebkitTransition: 'webkitTransitionEnd',
+    transition: 'transitionend'
+}[ transitionProp ];
+
+function setItemContentPixelSize( itemContent ) {
+    var previousContentSize = getSize( itemContent );
+    // disable transition
+    itemContent.style[ transitionProp ] = 'none';
+    // set current size in pixels
+    itemContent.style.width = previousContentSize.width + 'px';
+    itemContent.style.height = previousContentSize.height + 'px';
+}
+
+function addTransitionListener( itemContent ) {
+    if ( !transitionProp ) {
+        return;
+    }
+    // reset 100%/100% sizing after transition end
+    var onTransitionEnd = function() {
+        itemContent.style.width = '';
+        itemContent.style.height = '';
+        itemContent.removeEventListener( transitionEndEvent, onTransitionEnd );
+    };
+    itemContent.addEventListener( transitionEndEvent, onTransitionEnd );
+}
+
+function setItemContentTransitionSize( itemContent, itemElem ) {
+    // set new size
+    var size = getSize( itemElem );
+    itemContent.style.width = size.width + 'px';
+    itemContent.style.height = size.height + 'px';
+}
+
---
+++ js/skills.js
@@ -0,0 +1,99 @@
+var $skillproto = $($('.grid-item')[0]);
+var $skillgrid = $('#facts .grid');
+
+function generateSkillCards(){
+    for (const skill of skills){
+        var testcard = $skillproto.clone()
+        testcard.find('.card-title').text(skill.name);
+        testcard.find('.icon')[0].src = skill.img;
+        testcard[0].className = '';
+        for (classname of skill.tags){
+            testcard.addClass(classname);
+        }
+        $('.grid').append(testcard);
+    }
+}
+
+function insertSkillDescription(skillcard){
+    for (skill of skills){
+        if (skill.name == skillcard.innerText){
+            var desc = skill.desc;
+            break;
+        }
+    }
+    $(skillcard).find('.grid-card-description').text(desc);
+}
+
+var skills = {};
+skills = [
+    {"name":"C/C++","tags":["grid-item","mtrx","rev-eng","languages"],"img":"./images/icons8-c++-48.png",
+    "desc":""},
+    {"name":"Volatility","tags":["grid-item","forensics"],"img":"./images/icons8-detective-64.png",
+    "desc":""},
+    {"name":"x86 assembly","tags":["grid-item","plat-arch","mtrx","rev-eng","languages"],"img":"./images/icons8-microchip-48.png",
+    "desc":""},
+    {"name":"ARM assembly","tags":["grid-item","plat-arch","mtrx","rev-eng","languages"],"img":"./images/icons8-microchip-48.png",
+    "desc":""},
+    {"name":"Splunk","tags":["grid-item","ent-sec","ds-ml"],"img":"./images/splunk.png",
+    "desc":""},
+    {"name":"Azure","tags":["grid-item","ent-sec","plat-arch","ds-ml"],"img":"./images/icons8-azure-48.png",
+    "desc":""},
+    {"name":"CarbonBlack","tags":["grid-item","ent-sec"],"img":"./images/carbonblack.ico",
+    "desc":""},
+    {"name":"ServiceNow","tags":["grid-item","ent-sec"],"img":"./images/servicenow.png",
+    "desc":""},
+    {"name":"Windows internals","tags":["grid-item","rev-eng","forensics"],"img":"./images/icons8-windows-10-48.png",
+    "desc":""},
+    {"name":"Linux internals","tags":["grid-item","mtrx","rev-eng","forensics"],"img":"./images/icons8-linux-48.png",
+    "desc":""},
+    {"name":"VBA","tags":["grid-item","rev-eng","languages"],"img":"./images/864px-Microsoft_Office_2013_logo.png",
+    "desc":""},
+    {"name":"Circuit design","tags":["grid-item","mtrx"],"img":"./images/icons8-robot-64.png",
+    "desc":""},
+    {"name":"Mechanical design","tags":["grid-item","mtrx"],"img":"./images/icons8-robot-64.png",
+    "desc":""},
+    {"name":"Control systems","tags":["grid-item","mtrx"],"img":"./images/icons8-robot-64.png",
+    "desc":""},
+    {"name":"Reinforcement learning","tags":["grid-item","ds-ml","mtrx"],"img":"./images/icons8-artificial-intelligence-64.png",
+    "desc":""},
+    {"name":"Neural networks","tags":["grid-item","ds-ml"],"img":"./images/icons8-artificial-intelligence-64.png",
+    "desc":""},
+    {"name":"Arduino","tags":["grid-item","plat-arch","mtrx"],"img":"./images/icons8-arduino-48.png",
+    "desc":""},
+    {"name":"Raspberry pi","tags":["grid-item","plat-arch","mtrx"],"img":"./images/icons8-raspberry-pi-48.png",
+    "desc":""},
+    {"name":"ESP 32","tags":["grid-item","plat-arch","mtrx"],"img":"./images/icons8-microchip-48.png",
+    "desc":""},
+    {"name":"Java","tags":["grid-item","languages"],"img":"./images/icons8-java-48.png",
+    "desc":""},
+    {"name":"TensorFlow","tags":["grid-item","ds-ml"],"img":"./images/tensorflow.png",
+    "desc":""},
+    {"name":"Pytorch","tags":["grid-item","ds-ml"],"img":"./images/pytorch.svg",
+    "desc":""},
+    {"name":"MATLAB","tags":["grid-item","ds-ml","languages","mtrx"],"img":"./images/icons8-matlab-64.png",
+    "desc":""},
+    {"name":"Solidworks","tags":["grid-item","mtrx"],"img":"./images/icons8-solidworks-48.png",
+    "desc":""},
+    {"name":"IDA","tags":["grid-item"],"img":"./images/Ida6-logo.png",
+    "desc":""},
+    {"name":"Ghidra","tags":["grid-item","rev-eng"],"img":"./images/ghidra.png",
+    "desc":""},
+    {"name":"Wireshark","tags":["grid-item","rev-eng","forensics"],"img":"./images/Wireshark_icon.png",
+    "desc":""},
+    {"name":"STM 32","tags":["grid-item","plat-arch","mtrx"],"img":"./images/icons8-microchip-48.png",
+    "desc":""},
+    {"name":"JavaScript","tags":["grid-item","rev-eng","languages"],"img":"./images/icons8-javascript-48.png",
+    "desc":""},
+    {"name":"Android","tags":["grid-item","plat-arch","mtrx"],"img":"./images/icons8-android-os-48.png",
+    "desc":""},
+    {"name":"French","tags":["grid-item","languages"],"img":"./images/icons8-language-48.png",
+    "desc":""},
+    {"name":"Japanese","tags":["grid-item","languages"],"img":"./images/icons8-language-48.png",
+    "desc":""},
+    {"name":"Mandarin","tags":["grid-item","languages"],"img":"./images/icons8-language-48.png",
+    "desc":""},
+    {"name":"Powershell","tags":["grid-item","ent-sec","rev-eng","languages"],"img":"./images/icons8-windows-10-48.png",
+    "desc":""},
+    {"name":"Powershell","tags":["grid-item","ent-sec","rev-eng","languages"],"img":"./images/icons8-windows-10-48.png"
+    ,"desc":""}
+];
